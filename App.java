b698d258899207c1a2ec8b0bd5a652c456f407c9
Sara Ibarra <saraiba33@gmail.com>
2022-05-23 16:16:47 -0600

    Add solution

--- App.java
+++ App.java
@@ -1,6 +1,25 @@
 class App {
-  public static void main(String args[]){
-    String message = "Hello, world!";
-    System.out.println(message);
+  public static void main(String args[]) {
+    String vin = "VIN: 3123333";
+    System.out.println(vin);
+
+    String vehicle = "Make/Model: Ford Escape";
+    System.out.println(vehicle);
+
+    String paintColor = "Color: silver";
+    System.out.println(paintColor);
+
+    String towing = "Towing Package: No";
+    System.out.println(towing);
+
+    String odometer = "Odometer: 80,000 miles";
+    System.out.println(odometer);
+
+    String price = "Price: $8500";
+    System.out.println(price);
+
+    String qualityRating = "Quality Rating: B";
+    System.out.println(qualityRating);
+
   }
 }
